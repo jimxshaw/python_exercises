1281429f279cd4c60d2f2b2c6f7d5fe0d15cff4c
Jim Shaw <jimxshaw@gmail.com>
2023-07-22 14:00:32 -0500

    Add another reverse string method test.

--- reverse_string/test_reverse_string.py
+++ reverse_string/test_reverse_string.py
@@ -1,7 +1,7 @@
 import unittest
 import sys
 from io import StringIO
-from reverse_string import reverse_string_reversed, reverse_string_slice, reverse_string_loop
+from reverse_string import reverse_string_reversed, reverse_string_slice, reverse_string_loop, reverse_string_reduce
 
 class TestReverseString(unittest.TestCase):
     def test_reverse_string(self):
@@ -40,6 +40,19 @@ class TestReverseString(unittest.TestCase):
         # Multiple words.
         input = "Hello World!"
         actual = reverse_string_loop(input)        
+        expected = "!dlroW olleH"
+        self.assertEqual(actual, expected)
+
+    def test_reverse_string_reduce(self):
+        # One word.
+        input = "Johnny"
+        actual = reverse_string_reduce(input)
+        expected = "ynnhoJ"
+        self.assertEqual(actual, expected)
+
+        # Multiple words.
+        input = "Hello World!"
+        actual = reverse_string_reduce(input)        
         expected = "!dlroW olleH"
         self.assertEqual(actual, expected)
 
