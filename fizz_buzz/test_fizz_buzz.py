70085f014a857f649afd43274f26d3bcac82c650
Jim Shaw <jimxshaw@gmail.com>
2023-07-23 19:16:39 -0500

    Add fizz buzz implementation and test.

--- fizz_buzz/test_fizz_buzz.py
+++ fizz_buzz/test_fizz_buzz.py
@@ -1,5 +1,5 @@
 import unittest
-from fizz_buzz import fizz_buzz_while
+from fizz_buzz import fizz_buzz_while, fizz_buzz_range
 
 class TestFizzBuzz(unittest.TestCase):
   def test_fizz_buzz_while(self):
@@ -8,6 +8,14 @@ class TestFizzBuzz(unittest.TestCase):
     ]
 
     actual = fizz_buzz_while(20)
+    self.assertEqual(actual, expected)
+
+  def test_fizz_buzz_range(self):
+    expected = [
+      '1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', 'buzz', '11', 'fizz', '13', '14', 'fizzbuzz', '16', '17', 'fizz', '19', 'buzz'
+    ]
+
+    actual = fizz_buzz_range(20)
     self.assertEqual(actual, expected)
 
 if __name__ == "__main__":
