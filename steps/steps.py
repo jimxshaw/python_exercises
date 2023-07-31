c010dc7a13476ea5fd87392576a594fbed2b7353
Jim Shaw <jimxshaw@gmail.com>
2023-07-31 10:51:39 -0500

    Add steps recursion.

--- steps/steps.py
+++ steps/steps.py
@@ -1,3 +1,18 @@
 def steps(n):
   for i in range(1, n + 1):
     print('#' * i + ' ' * (n - i))
+
+def steps_recursion(n, row=0, step=''):
+  if n == row:
+    return
+
+  if n == len(step):
+    print(step)
+    return steps_recursion(n, row + 1)
+
+  if len(step) <= row:
+    step += '#'
+  else:
+    step += ' '
+
+  steps_recursion(n, row, step)
